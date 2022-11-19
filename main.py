6ae7b2de02592fb5fb483a5c2d409f3804cea498
phxgg <stamatis.1702@gmail.com>
2022-11-19 11:06:22 +0200

    new

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from time import sleep
 import platform
 import psutil
 import base64
+from os import system, name
 
 # suppress warnings
 # requests.packages.urllib3.disable_warnings()
@@ -14,11 +15,21 @@ disable_warnings()
 # global variables
 app_port = None
 auth_token = None
+riotclient_auth_token = None
+riotclient_app_port = None
 region = None
 lcu_name = None  # LeagueClientUx executable name
+lcu_api = None
+riotclient_api = None
+riotclient_headers = None
+lcu_headers = None
+lcu_session_token = None
+riotclient_session_token = None
 showNotInChampSelect = True
 
 # functions
+
+
 def getLCUName():
     '''
     Get LeagueClient executable name depending on platform.
@@ -40,7 +51,7 @@ def LCUAvailable():
 
 
 def getLCUArguments():
-    global auth_token, app_port, region
+    global auth_token, app_port, region, riotclient_auth_token, riotclient_app_port
     '''
     Get region, remoting-auth-token and app-port for LeagueClientUx.
     '''
@@ -58,71 +69,100 @@ def getLCUArguments():
                     auth_token = a.split('--remoting-auth-token=', 1)[1]
                 if '--app-port' in a:
                     app_port = a.split('--app-port=', 1)[1]
+                if '--riotclient-auth-token=' in a:
+                    riotclient_auth_token = a.split('--riotclient-auth-token=', 1)[1]
+                if '--riotclient-app-port=' in a:
+                    riotclient_app_port = a.split('--riotclient-app-port=', 1)[1]
+
+
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
 
 
 def main():
-    global showNotInChampSelect
-    
+    global showNotInChampSelect, lcu_api, riotclient_api, riotclient_headers, lcu_headers, lcu_session_token, riotclient_session_token
+
     # get LeagueClient name
     getLCUName()
 
     # get app port & auth token for each client
     getLCUArguments()
 
-    api = 'https://127.0.0.1:' + app_port
+    lcu_api = 'https://127.0.0.1:' + app_port
+    riotclient_api = 'https://127.0.0.1:' + riotclient_app_port
 
-    session_token = base64.b64encode(
+    lcu_session_token = base64.b64encode(
         ('riot:' + auth_token).encode('ascii')).decode('ascii')
 
-    headers = {
+    riotclient_session_token = base64.b64encode(
+        ('riot:' + riotclient_auth_token).encode('ascii')).decode('ascii')
+
+    lcu_headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        'Authorization': 'Basic ' + session_token
+        'Authorization': 'Basic ' + lcu_session_token
+    }
+
+    riotclient_headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'User-Agent': 'LeagueOfLegendsClient',
+        'Authorization': 'Basic ' + riotclient_session_token
     }
 
     # get current summoner
-    get_current_summoner = api + '/lol-summoner/v1/current-summoner'
+    get_current_summoner = lcu_api + '/lol-summoner/v1/current-summoner'
 
-    r = requests.get(get_current_summoner, headers=headers, verify=False)
+    r = requests.get(get_current_summoner, headers=lcu_headers, verify=False)
     r = json.loads(r.text)
 
     print('Connected: ' + r['displayName'])
 
-    # get champ select
-    while True:
-        get_champ_select = api + '/lol-champ-select/v1/session'
+    # get lobby
+    try :
+        checkForLobby = True
 
-        r = requests.get(get_champ_select, headers=headers, verify=False)
-        r = json.loads(r.text)
-
-        # if 'httpStatus' in r and r['httpStatus'] == 404:
-        if 'errorCode' in r:
-            if showNotInChampSelect:
-                print('Not in champ select. Waiting for game...')
-                showNotInChampSelect = False
-            sleep(2)
-        else:
-            print('\n* Found lobby. *\n')
-            # format_response = json.dumps(r, indent=2)
-            # print(format_response)
-
-            for x in r['myTeam']:
-                get_summoner_by_id = api + '/lol-summoner/v1/summoners/' + str(x['summonerId'])
-
-                r = requests.get(get_summoner_by_id, headers=headers, verify=False)
-                r = json.loads(r.text)
-
-                print(r['displayName'] + ' joined the lobby')
-
-            showNotInChampSelect = True
-            break
-
-    # keep main thread alive
-    try:
         while True:
-            sleep(1)
+            get_champ_select = lcu_api + '/lol-champ-select/v1/session'
+
+            r = requests.get(get_champ_select, headers=lcu_headers, verify=False)
+            r = json.loads(r.text)
+
+            # if 'httpStatus' in r and r['httpStatus'] == 404:
+            if 'errorCode' in r:
+                checkForLobby = True
+                if showNotInChampSelect:
+                    print('Not in champ select. Waiting for game...')
+                    showNotInChampSelect = False
+            else:
+                if checkForLobby:
+                    clear()
+                    print('\n* Found lobby. *\n')
+
+                    get_lobby = riotclient_api + '/chat/v5/participants/champ-select'
+
+                    r = requests.get(get_lobby, headers=riotclient_headers, verify=False)
+                    r = json.loads(r.text)
+
+                    # format_response = json.dumps(r, indent=2)
+                    # print(format_response)
+
+                    for x in r['participants']:
+                        print(x['game_name'] + ' joined the lobby')
+                    
+                    print('\n')
+
+                    showNotInChampSelect = True
+                    checkForLobby = False
+            sleep(2)
     except KeyboardInterrupt:
-        sys.exit()
+        print('\n\n* Exiting... *')
+        sys.exit(0)
 
 
 if __name__ == '__main__':
